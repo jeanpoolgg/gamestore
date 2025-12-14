0b3b801685c49e67bdfb2bcc499613b0847d2526
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-12-14 08:57:32 -0500

    feat: add plugins MU, Core and Blocks

---
+++ plugins/core-gamestore/core-gamestore.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name: Core Gamestore
+ * Description: Core functionalities for the Gamestore website.
+ * Version: 1.0.0
+ * Author: Jean Pool Gutierrez
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: core-gamestore
+ * Domain Path: /languages
+ */
+
+
+define('GAMESTORE_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('GAMESTORE_PLUGIN_PATH', plugin_dir_path(__FILE__));
